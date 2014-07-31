51fae3f71c691f3b35494fe3f8d8aa46032ebf7f
bayanijulian <bayanijulian@gmail.com>
2014-07-30 21:08:54 -0500

    Project Setup

---
+++ desktop/src/com/hypetrainstudios/dontcrash/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hypetrainstudios.dontcrash.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hypetrainstudios.dontcrash.DontCrash;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DontCrash(), config);
+	}
+}
